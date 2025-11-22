7bfd60ec44a589fab568d4b97fe6ed257745f055
Chris-github01 <90413281+Chris-github01@users.noreply.github.com>
2025-11-23 12:18:06 +1300

    Updated .node-version

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
   build: {
-    sourcemap: false,
-    minify: 'esbuild',
-    target: 'es2015',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
+    outDir: 'dist',
   },
-  server: {
-    port: 5173,
-  },
-});
+})
